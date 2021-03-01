0cbe133ea162279fb9f8343a324ab8963a66da73
BF <bf@tbwb.nl>
2021-03-01 20:20:59 +0100

    Add diffing monitor

--- Arduino/Monitor/Monitor.ino
+++ Arduino/Monitor/Monitor.ino
@@ -2,6 +2,11 @@
 #include "io.h"
 
 #define CLOCK   2
+#define DIFF    7   // when high, show any change on the bus
+
+byte data_shadow = 0;
+byte addr_hi_shadow = 0;
+byte addr_lo_shadow = 0;
 
 void setup() {
   Io.setup();
@@ -11,7 +16,9 @@ void setup() {
   Log.println("This is a 24-bit data monitor.");
   Log.println("(c) 2021 BF");
 
+  pinMode(DIFF, INPUT);
   pinMode(CLOCK, INPUT);
+
   attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
 }
 
@@ -22,6 +29,29 @@ static void onClock() {
   byte addr_hi = Io.addr_hi();
   byte addr_lo = Io.addr_lo();
 
+  printInfoLine(data, addr_hi, addr_lo);
+}
+
+void loop() {
+  if (digitalRead(DIFF) == HIGH) {
+    Io.read();
+
+    byte data = Io.data();
+    byte addr_hi = Io.addr_hi();
+    byte addr_lo = Io.addr_lo();
+
+    if (data != data_shadow || addr_lo != addr_lo_shadow || addr_hi != addr_hi_shadow) {
+      printInfoLine(data, addr_hi, addr_lo);
+
+      data_shadow = data;
+      addr_hi_shadow = addr_hi;
+      addr_lo_shadow = addr_lo;
+    }
+  }
+}
+
+
+static void printInfoLine(byte data, byte addr_hi, byte addr_lo) {
   int address = addr_hi << 8 |  addr_lo;
 
   Log.printf("CE = %d %s ", Io.chip_enable(), Io.chip_enable()==LOW?"RAM":"ROM");
@@ -43,12 +73,12 @@ static void onClock() {
     Log.printAscii(data);
   }
 
-  if (address == 0x7FFA) {
+  if (address == 0x7FF8) {
     Io.out1(data);
     Log.printf(" I/O 1: %02x", data);
   }
 
-  if (address == 0x7FFB) {
+  if (address == 0x7FF9) {
     Io.out2(data);
     Log.printf(" I/O 2: %02x", data);
   }
@@ -56,5 +86,3 @@ static void onClock() {
   Log.flush();
 }
 
-void loop() {
-}
