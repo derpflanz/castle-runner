03fb841666ede0b03e21f7e9187ac9fecf809be4
BF <bf@tbwb.nl>
2024-01-06 17:34:27 +0100

    Cleanup

--- GameCode/castlerunner.asm
+++ GameCode/castlerunner.asm
@@ -3,9 +3,8 @@
 
 ; DATA
 @SPLASH     "Castle Runner"       ; 13 characters, X=3 to middle it
-@COPYRIGHT  "2021"
+@COPYRIGHT  "2023"
 @GAMEOVER   "Game Over!"
-@INTERRUPTS "IRQ Works!"
 
 ; init
 SEI             ; Disable interrupts
@@ -17,181 +16,23 @@ TXS
 JSR :ResetDisplay
 JSR :InitDisplay
 JSR :ClearDisplay
+JSR :InitVideoRam
 
-LDA #$46            ; Set cursor to 0
-JSR :__comm_out
+LDX #$09
+LDY #$09
+JSR :GotoCharXY
+
 LDA #$00
-JSR :__data_out
-LDA #$00
-JSR :__data_out
-LDA #$42
-JSR :__comm_out
+STA $80
+LDA #$C0
+STA $81
+JSR :WriteString
 
 LDA 'X'
-JSR :__data_out
+STA $0200
 
+JSR :WriteCharScreen
 CLI             ; Enable interrupts
 
 :stop
 JMP :stop
-
-; Initialise memory
-; Steps left        = $f0          -> when this is 0: Game Over!
-; Gold collected    = $f1
-; Location row      = $f2
-; Location col      = $f3
-; Joystick          = $f4
-; New location row  = $f5
-; New location col  = $f6
-
-
-; Initialise memory
-LDA #$0a            ; we start with 10 steps and no gold
-STA $f0
-LDA #$00
-STA $f1
-LDA #$02
-STA $f2
-LDA #$0a
-STA $f3
-
-; Draw fixed content on screen
-LDA #$10
-JSR :DisplayGotoLocation
-LDA 'S'
-JSR :DisplayChar
-LDA #$24
-JSR :DisplayGotoLocation
-LDA 'G'
-JSR :DisplayChar
-
-:loop
-JSR :DrawRunner
-JSR :PrintGold
-JSR :PrintSteps
-LDA $f0
-BEQ :GameOver
-
-; Read Joystick
-LDA $4000
-CMP $f4
-BEQ :loop               ; joy was not changed
-
-LDA $4000
-STA $f4
-
-JSR :ClearRunner
-JSR :GetNewLocation
-JSR :MoveRunner
-JMP :loop
-
-:GameOver
-LDA #$01
-STA $80
-LDA #$05
-STA $81
-JSR :DisplayGotoRowCol
-LDA LO(@GAMEOVER)
-STA $80
-LDA HI(@GAMEOVER)
-STA $81
-JSR :DisplayString
-BRK
-
-:MoveRunner
-LDA $f5
-AND #$fc            ; we allow 4 rows: 0b00 to 0b11
-BEQ :__DoMoveRow
-RTS
-:__DoMoveRow
-LDA $f5
-STA $f2
-
-LDA $f6
-AND #$f0            ; we allow 16 columns: 0b0000 to 0b1111
-BEQ :__DoMoveColumn
-RTS
-:__DoMoveColumn
-LDA $f6
-STA $f3
-
-DEC $f0
-RTS
-
-:GetNewLocation
-LDA $f2             ; new location = current location
-STA $f5
-LDA $f3
-STA $f6
-
-LDA $f4
-AND #$08
-BNE :_cont1
-DEC $f6
-:_cont1
-LDA $f4
-AND #$10
-BNE :_cont2
-INC $f6
-:_cont2
-LDA $f4
-AND #$02
-BNE :_cont3
-DEC $f5
-:_cont3
-LDA $f4
-AND #$04
-BNE :_cont4
-INC $f5
-:_cont4
-RTS
-
-:ClearRunner
-LDA $f2
-STA $80
-LDA $f3
-STA $81
-JSR :DisplayGotoRowCol
-LDA ' '
-JSR :DisplayChar
-RTS
-
-:DrawRunner
-LDA $f2
-STA $80
-LDA $f3
-STA $81
-JSR :DisplayGotoRowCol
-LDA 'X'
-JSR :DisplayChar
-RTS
-
-:PrintSteps
-LDA #$00
-STA $80
-LDA #$11
-STA $81
-JSR :DisplayGotoRowCol
-LDA #$40
-STA $80
-LDA #$00
-STA $81
-LDA $f0
-JSR :Dec2Ascii
-JSR :DisplayString
-RTS
-
-:PrintGold
-LDA #$01
-STA $80
-LDA #$11
-STA $81
-JSR :DisplayGotoRowCol
-LDA #$40
-STA $80
-LDA #$00
-STA $81
-LDA $f1
-JSR :Dec2Ascii
-JSR :DisplayString
-RTS
