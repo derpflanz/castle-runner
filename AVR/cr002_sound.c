8affe31e2b8af2175b2d4e174b05f66fd011775a
BF <bf@tbwb.nl>
2025-03-08 16:59:56 +0100

    cleanups

--- AVR/cr002_sound.c
+++ AVR/cr002_sound.c
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 
 uint8_t c = 0;
-uint16_t c_f = 0;
 uint8_t dir = 2;
 uint16_t speed = 128;           // 128 is normal, lower is slower
 
@@ -25,19 +24,20 @@ uint16_t release_step;
 
 uint16_t end_of_attack, end_of_decay, end_of_sustain, end_of_release;
 
-
-uint16_t notes_oct4[] = {
-//  C    C#   D    D#   E    F    F#   G    G#   A    A#   B
-    138, 146, 154, 164, 173, 184, 194, 206, 218, 231, 245, 260
-};
-
-#define O4_G    206
-#define O4_A    231
+// The frequency number is a uint16_t and calculated as follows:
+// n = f / (f_clk / 256 / 32768)
+// n = f / 1.9073486328125
+// Octave 4, A is 440Hz
 #define O4_C    138
-#define O4_B    260
+#define O4_CS   146
 #define O4_D    154
-#define O4_F    184
+#define O4_DS   164
 #define O4_E    173
+#define O4_F    184
+#define O4_G    206
+#define O4_A    231         // 440 Hz
+#define O4_B    260
+
 #define REST    0
 #define END     65535
 
@@ -66,59 +66,31 @@ struct pair song[] = {
     { END,  1, 1, 0 , 1 }
 };
 
-char _sine[] = {
+// waveform values (0-255)
+uint8_t sine[] = {
     125, 128, 131, 134, 137, 140, 143, 146, 149, 152, 156, 159, 162, 165, 168, 171, 174, 176, 179, 182, 185, 188, 191, 193, 196, 199, 201, 204, 206, 209, 211, 213, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 237, 239, 240, 242, 243, 245, 246, 247, 248, 249, 250, 251, 252, 252, 253, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 253, 252, 252, 251, 250, 249, 248, 247, 246, 245, 243, 242, 240, 239, 237, 236, 234, 232, 230, 228, 226, 224, 222, 220, 218, 216, 213, 211, 209, 206, 204, 201, 199, 196, 193, 191, 188, 185, 182, 179, 176, 174, 171, 168, 165, 162, 159, 156, 152, 149, 146, 143, 140, 137, 134, 131, 128, 124, 121, 118, 115, 112, 109, 106, 103, 99, 96, 93, 90, 87, 84, 81, 79, 76, 73, 70, 67, 64, 62, 59, 56, 54, 51, 49, 46, 44, 42, 39, 37, 35, 33, 31, 29, 27, 25, 23, 21, 19, 18, 16, 15, 13, 12, 10, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 18, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 42, 44, 46, 49, 51, 54, 56, 59, 62, 64, 67, 70, 73, 76, 79, 81, 84, 87, 90, 93, 96, 99, 103, 106, 109, 112, 115, 118, 121, 124
 };
+uint8_t triangle[] = {
+    2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 254, 252, 250, 248, 246, 244, 242, 240, 238, 236, 234, 232, 230, 228, 226, 224, 222, 220, 218, 216, 214, 212, 210, 208, 206, 204, 202, 200, 198, 196, 194, 192, 190, 188, 186, 184, 182, 180, 178, 176, 174, 172, 170, 168, 166, 164, 162, 160, 158, 156, 154, 152, 150, 148, 146, 144, 142, 140, 138, 136, 134, 132, 130, 128, 126, 124, 122, 120, 118, 116, 114, 112, 110, 108, 106, 104, 102, 100, 98, 96, 94, 92, 90, 88, 86, 84, 82, 80, 78, 76, 74, 72, 70, 68, 66, 64, 62, 60, 58, 56, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0, 
+};
 
-uint8_t sine() {
-    c += 2;
-    return _sine[c];
-}
+// This sawtooth function is used to calculate a uint8_t value from the, 
+// uint16_t frequency, giving us more fine grained frequency steps (0-32768)
+// The return value can then be used in another waveform (e.g. sine[])
+uint8_t sawtooth(uint16_t step) {
+    static uint16_t wvalue = 0;
+    wvalue += step;
 
-uint8_t sawtooth(uint8_t s) {
-    c += s;
-    return c;
-}
-
-uint8_t sawtooth_f(uint16_t s) {
-    c_f += s;
-
-    if (c_f > 32768) {
-        c_f = 0;
+    if (wvalue > 32768) {
+        wvalue = 0;
     }
 
-    return (uint8_t) (c_f / 128);
-}
-
-uint8_t low_saw() {
-    static uint8_t subctr = 0;
-
-    subctr++;
-
-    if (subctr == 4) {
-        subctr = 0;
-        c++;
-    }
-
-    return c;
-}
-
-uint8_t triangle() {
-    if (c == 252) {
-        dir = -4;
-    } else if (c == 0) {
-        dir = 4;
-    }
-    
-    c += dir;
-    return c;
+    return (uint8_t) (wvalue / 128);
 }
 
 uint8_t noise() {
     uint8_t r = 0;
-
     r += rand();
-
     return r;
 }
 
@@ -127,26 +99,15 @@ ISR(TIMER0_OVF_vect) {
     cli();
     uint8_t n = 0;
 
-    // if (!(PINC & (1 << PC5))) {
-    //     n = sawtooth_f(462);
-    // }
-    
-    // if (!(PINC & (1 << PC4))) {
-    //     n = sawtooth_f(231);
-    //     n = _sine[n];
-    // }
-
     if (duration > 0) {
-        uint8_t waveform = sawtooth_f(freq);
-        //waveform = _sine[waveform];
+        uint8_t waveform_idx = sawtooth(freq);
+        n = triangle[waveform_idx];
 
-        uint16_t n_large = waveform * amplitude;
-        
+        uint16_t n_large = n * amplitude;
         n = n_large / 256;
     }
 
     OCR0A = n;
-
     sei();
 }
 
@@ -187,7 +148,7 @@ ISR(TIMER1_COMPA_vect) {
         }
         
         // speed 128 is normal, higher is faster
-        speed = 180;
+        speed = 250;
 
         current_note.attack = (current_note.attack * 128) / speed;
         current_note.decay = (current_note.decay * 128) / speed;
@@ -254,50 +215,10 @@ void init_duration_timer() {
 int main() {
     cli();
 
-    DDRC &= ~(1 << PC5);
-//    PORTC |= (1 << PC5);
-    DDRC |= (1 << PC0);
-    
-
     init_freq_timer();
     init_duration_timer();
 
     sei();
 
-    int prev_state = 0;
-    // tone_on = 4;
-    // TCNT1 = 0;
-    // freq = 231;             // A4
-    //duration = 2;
-
-    while (1) {
-//        uint8_t button_state = (PINC & (1 << PC5));
-
-    }
-
-    if (0) {
-        if (duration) {
-            PORTC |= (1 << PC0);         // Switch ON LED
-        } else {
-            PORTC &= ~(1 << PC0);
-        }
-
-        uint8_t button_state = (PINC & (1 << PC5));
-        if (button_state != prev_state) {
-            prev_state = button_state;
-
-            if (button_state == 0) {
-                if (note == 0) {
-                    duration = 2;
-                }
-                if (note == 1) {
-                    duration = 4;
-                    freq = 462;
-                }
-
-                TCNT1 = 0;
-                note++;
-            }
-        }
-    }
+    while (1);
 }
